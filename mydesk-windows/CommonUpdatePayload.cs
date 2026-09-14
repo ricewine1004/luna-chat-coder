5f918cf56f39de9a9b995f51306f8d59419c56ff
ricewine1004 <ricewine1004@gmail.com>
2026-09-14 13:52:40 +0900

    Set common update payload to 0.4.1

--- mydesk-windows/CommonUpdatePayload.cs
+++ mydesk-windows/CommonUpdatePayload.cs
@@ -2,15 +2,8 @@ namespace MyDeskAI.Windows;
 
 internal static class CommonUpdatePayload
 {
-    internal const string Version = "0.4.0";
+    internal const string Version = "0.4.1";
     internal static readonly string ZipBase64 =
         CommonUpdatePayloadPart01.Value +
-        CommonUpdatePayloadPart02.Value +
-        CommonUpdatePayloadPart03.Value +
-        CommonUpdatePayloadPart04.Value +
-        CommonUpdatePayloadPart05.Value +
-        CommonUpdatePayloadPart06.Value +
-        CommonUpdatePayloadPart07.Value +
-        CommonUpdatePayloadPart08.Value +
-        CommonUpdatePayloadPart09.Value;
+        CommonUpdatePayloadPartRest.Value;
 }
