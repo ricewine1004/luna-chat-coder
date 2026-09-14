db61f993ef2a92f2ce8f158a901147518fabc6b5
ricewine1004 <ricewine1004@gmail.com>
2026-09-14 11:58:51 +0900

    Bump common recovery update to 0.4.0

--- mydesk-windows/CommonUpdatePayload.cs
+++ mydesk-windows/CommonUpdatePayload.cs
@@ -2,7 +2,7 @@ namespace MyDeskAI.Windows;
 
 internal static class CommonUpdatePayload
 {
-    internal const string Version = "0.3.9";
+    internal const string Version = "0.4.0";
     internal static readonly string ZipBase64 =
         CommonUpdatePayloadPart01.Value +
         CommonUpdatePayloadPart02.Value +
