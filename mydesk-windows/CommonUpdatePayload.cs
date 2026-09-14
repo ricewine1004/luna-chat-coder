8e0770f6bbeb70d5da15bf8b1c85f24ed37711cb
ricewine1004 <ricewine1004@gmail.com>
2026-09-14 18:00:55 +0900

    Finalize 0.4.2 common payload manifest

--- mydesk-windows/CommonUpdatePayload.cs
+++ mydesk-windows/CommonUpdatePayload.cs
@@ -2,8 +2,12 @@ namespace MyDeskAI.Windows;
 
 internal static class CommonUpdatePayload
 {
-    internal const string Version = "0.4.1";
+    internal const string Version = "0.4.2";
+    internal const string Sha256 = "a89b30a2d3e788dc213430b83d8e0ce484db131f72a3baa157d3d1e01a896a2d";
     internal static readonly string ZipBase64 =
-        CommonUpdatePayloadPart01.Value +
-        CommonUpdatePayloadPartRest.Value;
+        CommonPatch042Part01.Value +
+        CommonPatch042Part02.Value +
+        CommonPatch042Part03.Value +
+        CommonPatch042Part04.Value +
+        CommonPatch042Part05.Value;
 }
