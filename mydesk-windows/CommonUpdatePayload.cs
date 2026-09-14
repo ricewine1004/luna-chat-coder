14514acf50a14806d6647f0356a4abdbf9584ad2
ricewine1004 <ricewine1004@gmail.com>
2026-09-14 18:01:27 +0900

    Use verified 0.4.2 two-part common payload

--- mydesk-windows/CommonUpdatePayload.cs
+++ mydesk-windows/CommonUpdatePayload.cs
@@ -5,9 +5,6 @@ internal static class CommonUpdatePayload
     internal const string Version = "0.4.2";
     internal const string Sha256 = "a89b30a2d3e788dc213430b83d8e0ce484db131f72a3baa157d3d1e01a896a2d";
     internal static readonly string ZipBase64 =
-        CommonPatch042Part01.Value +
-        CommonPatch042Part02.Value +
-        CommonPatch042Part03.Value +
-        CommonPatch042Part04.Value +
-        CommonPatch042Part05.Value;
+        CommonUpdatePayloadPart01.Value +
+        CommonUpdatePayloadPartRest.Value;
 }
